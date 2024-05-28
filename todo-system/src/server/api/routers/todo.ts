93d661303b4af6b4f0d70dcc1026d7d8086d4c79
Ploydy <Lfecarballo20@gmail.com>
2024-05-29 02:57:20 +0800

    initialize discord login

--- todo-system/src/server/api/routers/todo.ts
+++ todo-system/src/server/api/routers/todo.ts
@@ -1,25 +1,79 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { todoInput } from "~/types";
 
 export const todoRouter = createTRPCRouter({
-  getSecretMessage: protectedProcedure.query(() => {
+  all: protectedProcedure.query(async({ctx}) => {
+    const todos = await ctx.db.todo.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    console.log(
+      'todos from prisma',
+      todos.map(({id, text, done }) => ({id, text, done }))
+    );
     return [
       {
         id: 'fake1',
-        text: 'fake text',
+        text: 'fake text1',
         done: false,
       },
       {
         id: 'fake2',
-        text: 'fake text',
+        text: 'fake text2',
         done: true,
       },
       {
         id: 'fake3',
-        text: 'fake text',
+        text: 'fake text3',
         done: false,
       }
     ]
   }),
+  create: protectedProcedure
+  .input(todoInput)
+  .mutation(async ({ ctx, input }) => {
+    return ctx.db.todo.create({
+      data: {
+        text: input,
+        done: true,
+        user: {
+          connect: {
+            id: ctx.session.user.id,
+          }
+        }
+      }
+    })
+  }),
+  delete: protectedProcedure
+  .input(z.string())
+  .mutation(async ({ ctx, input }) => {
+    return ctx.db.todo.delete({
+      where: {
+        id: input,
+      }
+    })
+  }),
+  toggle: protectedProcedure
+  .input(z.object({
+    id: z.string(),
+    done: z.boolean(),
+  })
+)
+  .mutation(async ({ ctx, input: {id, done} }) => {
+    return ctx.db.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        done,
+      }
+    })
+  })
 });
--- todo-system/src/server/auth.ts
+++ todo-system/src/server/auth.ts
@@ -4,10 +4,10 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 import { type Adapter } from "next-auth/adapters";
-import EmailProvider from 'next-auth/providers/email'
-import { env } from "~/env";
 import { db } from "~/server/db";
+import { env } from "~/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -47,24 +47,19 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER ?? 'https://localhost:3000',
-        port: 587,
-        auth: {
-          user: 'apikey',
-          pass: process.env.EMAIL_PASSWORD ?? ''
-        },
-      },
-      from: process.env.EMAIL_FROM ?? 'default@default.com',
-      ...(process.env.NODE_ENV === 'production'
-      ? {
-        sendVerificationRequest({url}) {
-          console.log('LOGIN LINK', url)
-        },
-      }
-    : {}),
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
   ],
 };
 
@@ -74,3 +69,5 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = () => getServerSession(authOptions);
+
+
